256f7da4f3d7bf8137c54982e198c4ff836eb594
JamTheFrogg <faresrnja@gmail.com>
2026-09-12 18:11:30 +0200

    eo

--- server/policy/blacklist.ts
+++ server/policy/blacklist.ts
@@ -1,7 +1,7 @@
 import { isDestructiveCommand } from '../engine/classify.ts'
 import { parseCommand } from '../engine/parse.ts'
 import { longestPrefix, normalizeCmd } from './prefix.ts'
-import { loadPolicies } from './store.ts'
+import { loadPolicies } from '../policy/store.ts'
 
 function bannedBins(command: string, bins: string[]): boolean {
   const banned = new Set(bins.map((b) => b.toLowerCase()))
